73b679210d06656bb04d81a7d80f5133fd83b1b6
Paulo Bichara <contato@paulobichara.org>
2019-02-05 14:55:56 -0300

    Best so far. Not working with coursera for some reason.

--- src/main/java/PointsAndSegments.java
+++ src/main/java/PointsAndSegments.java
@@ -2,196 +2,167 @@ import java.util.Scanner;
 
 public class PointsAndSegments {
 
-    static class Range implements Comparable<Range> {
-        private int start, end;
-
-        Range(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-
-        @Override
-        public int compareTo(Range other) {
-            if (other == null) {
-                throw new NullPointerException("Other value is null");
-            }
-
-            return this.start > other.start || (this.start == other.start && this.end > other.end) ? 1
-                    : this.start < other.start || this.end < other.end ? -1 : 0;
-        }
-    }
-
-    /**
-     * Class used to get the sorted indexes order of the points without modifying the original array (merge sort)
-     */
-    static class PointsSorter {
-        static void sort(int[] array, int[] sortedIndexes, int left, int right) {
-            if (right <= left + 1) {
-                sortedIndexes[left] = left;
-                return;
-            }
-
-            int middle = (left + right) / 2;
-            sort(array, sortedIndexes, left, middle);
-            sort(array, sortedIndexes, middle, right);
-            merge(array, sortedIndexes, left, middle, right);
-        }
-
-        private static void merge(int[] values, int[] sortedIndexes, int leftBound, int middle, int rightBound) {
-            int totalElements = rightBound - leftBound;
-
-            int[] original = new int[totalElements];
-            System.arraycopy(sortedIndexes, leftBound, original, 0, rightBound - leftBound);
-
-            int nextLeftIndex = leftBound;
-            int nextRightIndex = middle;
-
-            for (int i = 0; i < totalElements; i++) {
-                if (nextLeftIndex < middle) {
-                    int nextLeft = values[original[nextLeftIndex - leftBound]];
-                    if (nextRightIndex < rightBound
-                            && (values[original[nextRightIndex - leftBound]] < nextLeft)) {
-                        sortedIndexes[i + leftBound] = original[nextRightIndex - leftBound];
-                        nextRightIndex++;
-                    } else {
-                        sortedIndexes[i + leftBound] = original[nextLeftIndex - leftBound];
-                        nextLeftIndex++;
-                    }
-                } else {
-                    sortedIndexes[i + leftBound] = original[nextRightIndex - leftBound];
-                    nextRightIndex++;
-                }
-            }
-        }
-    }
-
     /**
      * Class used to get the payoffs for the provided input data (quick sort)
      */
-    private static class RangesSorter {
+    private static class QuickSort {
 
-        private class Partitions {
+        private static class Partitions {
             int start;
             int end;
         }
 
-        private int[] points;
-        private int[] sortedIndexes;
-
-        private int firstSortedIndex;
-        private int lastSortedIndex;
-
-        RangesSorter(int[] points, int[] sortedIndexes) {
-            this.points = points;
-            this.sortedIndexes = sortedIndexes;
-        }
-
-        void sortRanges(Range[] ranges) {
-            if (ranges.length > 1) {
-                firstSortedIndex = 0;
-                lastSortedIndex = ranges.length - 1;
-                sortRanges(ranges, 0, ranges.length);
+        static void sort(int[] array) {
+            if (array.length > 1) {
+                sort(array, 0, array.length);
             }
         }
 
-        private void sortRanges(Range[] ranges, int left, int right) {
+        private static void sort(int[] array, int left, int right) {
             if (left + 1 >= right) {
                 return;
             }
 
-            switchValues(ranges, left, (right / 2) + left - 1);
+            switchValues(array, left, choosePivotRange(array, left, right));
 
-            Partitions partitions = partitionInThree(ranges, left, right);
-            Range pivotRange = ranges[partitions.start];
-
-            int firstPoint = getPointValue(0);
-            if (firstPoint > pivotRange.end) {
-                // First point after pivot range
-                firstSortedIndex = partitions.end + 1;
-            }
-
-            int lastPoint = getPointValue(sortedIndexes.length - 1);
-            if (lastPoint < pivotRange.start) {
-                // Last point before pivot range
-                lastSortedIndex = partitions.start - 1;
-            }
-
-            if (firstSortedIndex == left && lastSortedIndex == right - 1) {
-                sortRanges(ranges, left, partitions.start);
-                sortRanges(ranges, partitions.end + 1, right);
-            } else {
-                sortRanges(ranges, firstSortedIndex, lastSortedIndex + 1);
-            }
-
+            Partitions partitions = partitionInThree(array, left, right);
+            sort(array, left, partitions.start);
+            sort(array, partitions.end + 1, right);
         }
 
-        private Partitions partitionInThree(Range[] a, int left, int right) {
+        private static int choosePivotRange(int[] array, int left, int right) {
+            int[] candidates = new int[] { array[left], array[(right / 2) - 1], array[right - 1] };
+
+            int bestIndex = left;
+
+            int best = findBestPivotCandidate(candidates);
+            if (best == candidates[1]) {
+                bestIndex = (right / 2) - 1;
+            } else if (best == candidates[2]) {
+                bestIndex = right - 1;
+            }
+
+            return bestIndex;
+        }
+
+        private static int findBestPivotCandidate(int[] candidates) {
+            int best = candidates[0];
+            if (candidates[0] > candidates[1]) {
+                if (candidates[0] > candidates[2]) {
+                    if (candidates[1] < candidates[2]) {
+                        best = candidates[2];
+                    } else {
+                        best = candidates[1];
+                    }
+                }
+            } else if (candidates[1] > candidates[2]) {
+                if (candidates[0] < candidates[2]) {
+                    best = candidates[2];
+                }
+            } else {
+                best = candidates[1];
+            }
+            return best;
+        }
+
+        private static Partitions partitionInThree(int[] array, int left, int right) {
             Partitions partitions = new Partitions();
-            partitions.start = partitionInTwo(a, left, right, false);
-            partitions.end = partitionInTwo(a, partitions.start, right, true);
+            partitions.start = partitionInTwo(array, left, right, false);
+            partitions.end = partitionInTwo(array, partitions.start, right, true);
             return partitions;
         }
 
-        private int partitionInTwo(Range[] ranges, int left, int right, boolean invertEquals) {
-            Range pivot = ranges[left];
+        private static int partitionInTwo(int[] array, int left, int right, boolean invertEquals) {
+            int pivot = array[left];
             int pivotIndex = left;
 
             for (int i = left + 1; i < right; i++) {
-                Range currentRange = ranges[i];
-                if ((currentRange.compareTo(pivot) < 0  && !invertEquals) || (currentRange.compareTo(pivot) <= 0 && invertEquals)) {
+                int current = array[i];
+                if ((current < pivot  && !invertEquals) || (current <= pivot && invertEquals)) {
                     pivotIndex = pivotIndex + 1;
-                    switchValues(ranges, i, pivotIndex);
+                    switchValues(array, i, pivotIndex);
                 }
             }
-            switchValues(ranges, left, pivotIndex);
+            switchValues(array, left, pivotIndex);
             return pivotIndex;
         }
 
-        private int[] calculatePayoffs(int[] points, int[] sortedIndexes, Range[] ranges) {
-            int[] payoffs = new int[points.length];
-            int firstReachable = 0;
-
-            for (int rangeIndex = firstSortedIndex; rangeIndex <= lastSortedIndex; rangeIndex++) {
-                Range currentRange = ranges[rangeIndex];
-
-                for (int i = firstReachable; (i < points.length && points[sortedIndexes[i]] < currentRange.start); i++) {
-                    firstReachable++;
-                }
-
-                for (int i = firstReachable; i < points.length && points[sortedIndexes[i]] <= currentRange.end; i++) {
-                    payoffs[sortedIndexes[firstReachable]]++;
-                }
-            }
-
-            return  payoffs;
-        }
-
-        private void switchValues(Range[] array, int from, int to) {
-            Range oldValue = array[from];
+        private static void switchValues(int[] array, int from, int to) {
+            int oldValue = array[from];
             array[from] = array[to];
             array[to] = oldValue;
         }
+    }
 
-        private int getPointValue(int index) {
-            return points[sortedIndexes[index]];
+    private static class BinarySearch {
+
+        int qtyPointsBeforeOrEqual;
+        int qtyPointsAfterOrEqual;
+
+        void countPointsBeforeAndAfter(int[] array, int key) {
+            int middleIndex = -1, middle = 0;
+
+            this.qtyPointsBeforeOrEqual = 0;
+            this.qtyPointsAfterOrEqual = 0;
+
+            for (int leftIndex = 0, rightIndex = array.length - 1; leftIndex <= rightIndex;) {
+                middleIndex = ((rightIndex - leftIndex) / 2) + leftIndex;
+                middle = array[middleIndex];
+                if (key < middle) {
+                    rightIndex = middleIndex - 1;
+                } else if (key > middle) {
+                    leftIndex = middleIndex + 1;
+                } else {
+                    break;
+                }
+            }
+
+            if (middleIndex != -1 && middle == key) {
+                int firstEqual = middleIndex, lastEqual = middleIndex;
+                for (int index = middleIndex - 1; index >= 0 && array[index] == middle; index--) {
+                    firstEqual = index;
+                }
+                for (int index = middleIndex + 1; index <= array.length - 1 && array[index] == middle; index++) {
+                    lastEqual = index;
+                }
+                this.qtyPointsBeforeOrEqual = lastEqual + 1;
+                this.qtyPointsAfterOrEqual = array.length - firstEqual;
+            } else {
+                if (key < middle) {
+                    this.qtyPointsBeforeOrEqual = middleIndex;
+                } else {
+                    this.qtyPointsBeforeOrEqual = middleIndex + 1;
+                }
+                this.qtyPointsAfterOrEqual = array.length - this.qtyPointsBeforeOrEqual;
+            }
         }
     }
 
-    static int[] fastCountSegments(Range[] ranges, int[] points) {
-        int[] sortedPointsIndexes = new int[points.length];
-        PointsSorter.sort(points, sortedPointsIndexes, 0, points.length);
+    static int[] fastCountSegments(int[] starts, int[] ends, int[] points) {
+        QuickSort.sort(starts);
+        QuickSort.sort(ends);
 
-        RangesSorter rangesSorter = new RangesSorter(points, sortedPointsIndexes);
-        rangesSorter.sortRanges(ranges);
+        BinarySearch search = new BinarySearch();
 
-        return rangesSorter.calculatePayoffs(points, sortedPointsIndexes, ranges);
+        int[] payoffs = new int[points.length];
+        for (int pointIndex = 0; pointIndex < points.length; pointIndex++) {
+            search.countPointsBeforeAndAfter(starts, points[pointIndex]);
+            int qtyStartsBefore = search.qtyPointsBeforeOrEqual;
+            search.countPointsBeforeAndAfter(ends, points[pointIndex]);
+            int qtyEndsAfter = search.qtyPointsAfterOrEqual;
+
+            int payoff = qtyStartsBefore + qtyEndsAfter - starts.length;
+            payoffs[pointIndex] = payoff < 0 ? 0 : payoff;
+        }
+
+        return payoffs;
     }
 
-    static int[] naiveCountSegments(Range[] ranges, int[] points) {
+    static int[] naiveCountSegments(int[] starts, int[] ends, int[] points) {
         int[] cnt = new int[points.length];
         for (int i = 0; i < points.length; i++) {
-            for (int j = 0; j < ranges.length; j++) {
-                if (ranges[j].start <= points[i] && points[i] <= ranges[j].end) {
+            for (int j = 0; j < starts.length; j++) {
+                if (starts[j] <= points[i] && points[i] <= ends[j]) {
                     cnt[i]++;
                 }
             }
@@ -204,16 +175,18 @@ public class PointsAndSegments {
         int n, m;
         n = scanner.nextInt();
         m = scanner.nextInt();
-        Range[] ranges = new Range[n];
+        int[] starts = new int[n];
+        int[] ends = new int[n];
         int[] points = new int[m];
         for (int i = 0; i < n; i++) {
-            ranges[i] = new Range(scanner.nextInt(), scanner.nextInt());
+            starts[i] = scanner.nextInt();
+            ends[i] = scanner.nextInt();
         }
         for (int i = 0; i < m; i++) {
             points[i] = scanner.nextInt();
         }
         //use fastCountSegments
-        int[] cnt = fastCountSegments(ranges, points);
+        int[] cnt = fastCountSegments(starts, ends, points);
         for (int x : cnt) {
             System.out.print(x + " ");
         }
--- src/test/java/PointsAndSegmentsTest.java
+++ src/test/java/PointsAndSegmentsTest.java
@@ -1,48 +1,89 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Random;
+
 public class PointsAndSegmentsTest {
 
     private static final String ERROR_MESSAGE = "Wrong Answer for: \n";
 
-    @Test
-    public void testPointsSorter() {
-        testArray(new int[]{ 0 }, new int[] { 0 });
-        testArray(new int[] { 1, 2 }, new int[] { 0, 1 });
-        testArray(new int[]{ 2, 1 }, new int[] { 1, 0 });
-        testArray(new int[]{ 1, 2, 3 }, new int[] { 0, 1, 2 });
-        testArray(new int[]{ 1, 3, 2 }, new int[] { 0, 2, 1 });
-        testArray(new int[]{ 2, 1, 3 }, new int[] { 1, 0, 2 });
-        testArray(new int[]{ 2, 3, 1 }, new int[] { 2, 0, 1 });
-        testArray(new int[]{ 3, 2, 1 }, new int[] { 2, 1, 0 });
-        testArray(new int[]{ 1, 2, 3, 4 }, new int[] { 0, 1, 2, 3 });
-        testArray(new int[]{ 1, 3, 2, 4 }, new int[] { 0, 2, 1, 3 });
-        testArray(new int[]{ 2, 1, 3, 4 }, new int[] { 1, 0, 2, 3 });
-        testArray(new int[]{ 1, 2, 4, 3 }, new int[] { 0, 1, 3, 2 });
-        testArray(new int[]{ 3, 2, 1, 4 }, new int[] { 2, 1, 0, 3 });
-        testArray(new int[]{ 4, 3, 2, 1 }, new int[] { 3, 2, 1, 0 });
-        testArray(new int[]{ 2, 3, 9, 2, 9 }, new int[] { 0, 3, 1, 2, 4 });
-    }
+    private static final int STRESS_ARRAY_MAX_SIZE = 10;
+    private static final int STRESS_MAX_NUMBER = 100000000;
+
+    private static final Random RANDOM = new SecureRandom();
 
     @Test
     public void testSamples() {
-        PointsAndSegments.Range[] ranges = new PointsAndSegments.Range[] { createRange(0, 5), createRange(7, 10) };
-        Assert.assertArrayEquals(ERROR_MESSAGE, new int[] { 1, 0, 0 }, PointsAndSegments.fastCountSegments(ranges, new int[] { 1, 6, 11 }));
+        Assert.assertArrayEquals(ERROR_MESSAGE, new int[] { 1, 0, 0 },
+                PointsAndSegments.fastCountSegments(new int[] { 0, 7 }, new int[] { 5, 10 }, new int[] { 1, 6, 11 }));
 
-        ranges = new PointsAndSegments.Range[] { createRange( -10, 10) };
-        Assert.assertArrayEquals(ERROR_MESSAGE, new int[] { 0, 0, 1 }, PointsAndSegments.fastCountSegments(ranges, new int[] { -100, 100, 0 }));
+        Assert.assertArrayEquals(ERROR_MESSAGE, new int[] { 0, 0, 1 },
+                PointsAndSegments.fastCountSegments(new int[] { -10 }, new int[] { 10 }, new int[] { -100, 100, 0 }));
 
-        ranges = new PointsAndSegments.Range[] { createRange( 0, 5 ), createRange( -3, 2 ), createRange( 7, 10 ) };
-        Assert.assertArrayEquals(ERROR_MESSAGE, new int[] { 2, 0 }, PointsAndSegments.fastCountSegments(ranges, new int[] { 1, 6 }));
+        Assert.assertArrayEquals(ERROR_MESSAGE, new int[] { 2, 0 },
+                PointsAndSegments.fastCountSegments(new int[] { 0, -3, 7 }, new int[] { 5, 2, 10 }, new int[] { 1, 6 }));
+
+        Assert.assertArrayEquals(ERROR_MESSAGE, new int[] { 1, 2, 1, 1 },
+                PointsAndSegments.fastCountSegments(new int[] { 2, 3, 5, 7 }, new int[] { 3, 5, 5, 7 }, new int[] { 4, 3, 2, 7 }));
+
+        Assert.assertArrayEquals(ERROR_MESSAGE, new int[] { 0, 0, 0 },
+                PointsAndSegments.fastCountSegments(new int[] { 4, 6, 7, 9 }, new int[] { 4, 5, 5, 8 }, new int[] { 8, 6, 0 }));
+
+        Assert.assertArrayEquals(ERROR_MESSAGE, new int[] { 0, 0, 0 },
+                PointsAndSegments.fastCountSegments(new int[] { 1, 1 }, new int[] { 2, 2 }, new int[] { 8, 6, 0 }));
+
+        Assert.assertArrayEquals(ERROR_MESSAGE, new int[] { 1, 1, 0 },
+                PointsAndSegments.fastCountSegments(new int[] { 1 }, new int[] { 2 }, new int[] { 1, 2, 3 }));
+
+        Assert.assertArrayEquals(ERROR_MESSAGE, new int[] { 1, 0, 0 },
+                PointsAndSegments.fastCountSegments(new int[] { 1 }, new int[] { 1 }, new int[] { 1, 2, 3 }));
+
+        Assert.assertArrayEquals(ERROR_MESSAGE, new int[] { 1 },
+                PointsAndSegments.fastCountSegments(new int[] { 0 }, new int[] { 0 }, new int[] { 0 }));
+
+        Assert.assertArrayEquals(ERROR_MESSAGE, new int[] { 0, 1, 2, 1, 0 },
+                PointsAndSegments.fastCountSegments(new int[] { 0, 2 }, new int[] { 2, 5 }, new int[] { -1, 1, 2, 5, 6 }));
+
+        Assert.assertArrayEquals(ERROR_MESSAGE, new int[] { 0, 4, 4, 0 },
+                PointsAndSegments.fastCountSegments(new int[] { 0, 0, 0, 0 }, new int[] { 1, 1, 1, 1 }, new int[] { -1, 0, 1, 2 }));
+
+        Assert.assertArrayEquals(ERROR_MESSAGE, new int[] { 0, 0, 1 },
+                PointsAndSegments.fastCountSegments(new int[] { 1, 2, 2 }, new int[] { 3, 3, 4 }, new int[] { -1, 0, 1 }));
+
+        Assert.assertArrayEquals(ERROR_MESSAGE, new int[] { 1 },
+                PointsAndSegments.fastCountSegments(new int[] { 1 }, new int[] { 1 }, new int[] { 1 }));
+
+        Assert.assertArrayEquals(ERROR_MESSAGE, new int[] { 0 },
+                PointsAndSegments.fastCountSegments(new int[] { 2 }, new int[] { 2 }, new int[] { 1 }));
     }
 
-    private PointsAndSegments.Range createRange(int start, int end) {
-        return new PointsAndSegments.Range(start, end);
+    @Test
+    public void stressTest() {
+        long startTime = System.nanoTime();
+        double duration = 0;
+        while (true) {
+            int[] starts = RANDOM.ints(RANDOM.nextInt(STRESS_ARRAY_MAX_SIZE), 0, STRESS_MAX_NUMBER).toArray();
+            int[] ends = RANDOM.ints(starts.length, -STRESS_MAX_NUMBER, STRESS_MAX_NUMBER).toArray();
+            int[] points = RANDOM.ints(RANDOM.nextInt(STRESS_ARRAY_MAX_SIZE), 0, STRESS_MAX_NUMBER).toArray();
+            int[] payoffsFast = PointsAndSegments.fastCountSegments(starts, ends, points);
+            int[] payoffsNaive = PointsAndSegments.naiveCountSegments(starts, ends, points);
+
+            Assert.assertArrayEquals(generateErrorMessage(starts, ends, points, payoffsNaive, payoffsFast), payoffsNaive, payoffsFast);
+            duration = (System.nanoTime() - startTime) / 1_000_000_000.0;
+        }
     }
 
-    private void testArray(int[] array, int[] expected) {
-        int[] sortedIndexes = new int[array.length];
-        PointsAndSegments.PointsSorter.sort(array, sortedIndexes, 0, array.length);
-        Assert.assertArrayEquals(expected, sortedIndexes);
+    private String generateErrorMessage(int[] starts, int[] ends, int[] points, int[] expected, int[] result) {
+        String message = ERROR_MESSAGE + starts.length + " " + points.length + "\n";
+        for (int i = 0; i < starts.length; i++) {
+            message = message + starts[i] + " " + ends[i] + "\n";
+        }
+        for (int i = 0; i < points.length; i++) {
+            message = message + points[i] + " ";
+        }
+        return message + "\nExpected: " + Arrays.toString(expected) + " and got: " + Arrays.toString(result);
     }
+
 }
