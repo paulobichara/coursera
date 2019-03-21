a3b3d74afa3bdcb88a8a0a82971eb803b9e467bb
Paulo Bichara <contato@paulobichara.org>
2019-03-21 15:08:08 -0300

    Best so far

--- src/main/java/PhoneBookProblem.java
+++ src/main/java/PhoneBookProblem.java
@@ -1,9 +1,106 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.security.SecureRandom;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
 import java.util.StringTokenizer;
+
+class Contact {
+    int number;
+    String name;
+
+    Contact(int number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+}
+
+class PhoneBook {
+    class HashFunction {
+        static final int PRIME = 10000019;
+
+        BigInteger cardinality;
+        BigInteger param1;
+        BigInteger param2;
+
+        HashFunction(int cardinality) {
+            this.cardinality = BigInteger.valueOf(cardinality);
+
+            Random random = new Random(System.currentTimeMillis());
+            this.param1 = BigInteger.valueOf(random.nextInt(PRIME - 1) + 1);
+            this.param2 = BigInteger.valueOf(random.nextInt(PRIME));
+        }
+
+        int calculateHash(int number) {
+            return param1.multiply(BigInteger.valueOf(number)).add(param2).mod(BigInteger.valueOf(PRIME))
+                .mod(cardinality).intValueExact();
+        }
+    }
+
+    private static final int SIZE = 100000;
+
+    private Contact[] table;
+
+    private HashFunction hashFunction;
+
+    PhoneBook() {
+        table = new Contact[SIZE];
+        hashFunction = new HashFunction(SIZE);
+    }
+
+    void add(Contact contact) {
+        table[hashFunction.calculateHash(contact.number)] = contact;
+    }
+
+    void del(int number) {
+        table[hashFunction.calculateHash(number)] = null;
+    }
+
+    Contact find(int number) {
+        return table[hashFunction.calculateHash(number)];
+    }
+}
+
+class PhoneBookNaive {
+
+    // Keep list of all existing (i.e. not deleted yet) contacts.
+    private List<Contact> contacts = new ArrayList<>();
+
+    void add(int number, String name) {
+        // if we already have contact with such number,
+        // we should rewrite contact's name
+        boolean wasFound = false;
+        for (Contact contact : contacts)
+            if (contact.number == number) {
+                contact.name = name;
+                wasFound = true;
+                break;
+            }
+        // otherwise, just add it
+        if (!wasFound)
+            contacts.add(new Contact(number, name));
+    }
+
+    void del(int number) {
+        for (Iterator<Contact> it = contacts.iterator(); it.hasNext(); )
+            if (it.next().number == number) {
+                it.remove();
+                break;
+            }
+    }
+
+    Contact find(int number) {
+        for (Contact contact : contacts)
+            if (contact.number == number) {
+                return contact;
+            }
+
+        return null;
+    }
+}
 
 public class PhoneBookProblem {
 
@@ -31,63 +128,6 @@ public class PhoneBookProblem {
         }
     }
 
-    static class Contact {
-        int number;
-        String name;
-
-        Contact(int number, String name) {
-            this.number = number;
-            this.name = name;
-        }
-    }
-
-    static class HashFunction {
-        static final int PRIME = 10000019;
-
-        int cardinality;
-        long param1;
-        long param2;
-
-        HashFunction(int cardinality) {
-            this.cardinality = cardinality;
-
-            Random random = new SecureRandom();
-            this.param1 = random.nextInt(PRIME - 1) + 1;
-            this.param2 = random.nextInt(PRIME);
-        }
-
-        int calculateHash(int number) {
-            return (int) (((param1 * number + param2) % PRIME) % cardinality);
-        }
-    }
-
-    static class PhoneBook {
-        static final int SIZE = 100000;
-
-        Contact[] table;
-        int numKeys;
-
-        HashFunction hashFunction;
-
-        PhoneBook() {
-            numKeys = 0;
-            table = new Contact[SIZE];
-            hashFunction = new HashFunction(SIZE);
-        }
-
-        void add(Contact contact) {
-            table[hashFunction.calculateHash(contact.number)] = contact;
-        }
-
-        void remove(int number) {
-            table[hashFunction.calculateHash(number)] = null;
-        }
-
-        Contact find(int number) {
-            return table[hashFunction.calculateHash(number)];
-        }
-    }
-
     public static void main(String[] args) {
         FastScanner in = new FastScanner();
         int queryCount = in.nextInt();
@@ -103,7 +143,7 @@ public class PhoneBookProblem {
                     System.out.println(contact == null ? "not found" : contact.name);
                     break;
                 case "del":
-                    phoneBook.remove(in.nextInt());
+                    phoneBook.del(in.nextInt());
                     break;
             }
         }
