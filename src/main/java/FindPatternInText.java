eca68dc9d7bdebbf183d66c727f2ad674c4451bc
Paulo Bichara <contato@paulobichara.org>
2019-03-23 11:36:10 -0300

    Best so far

---
+++ src/main/java/FindPatternInText.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class FindPatternInText {
+
+    static class HashFunction {
+        static final int PRIME = 1_000_000_007;
+        static final int CARDINALITY = 1_000_000_000;
+        static final int X_FACTOR = 263;
+
+        int computeHash(String value) {
+            long hash = 0;
+            for (int i = value.length() - 1; i >= 0; --i)
+                hash = (hash * X_FACTOR + value.charAt(i)) % PRIME;
+            return (int)hash % CARDINALITY;
+        }
+
+        int[] preComputeHashes(String pattern, String text) {
+            int lastSubStrIndex = text.length() - pattern.length();
+            int[] hashes = new int[ text.length() - pattern.length() + 1 ];
+            hashes[text.length() - pattern.length()] = computeHash(text.substring(lastSubStrIndex));
+
+            long y = 1;
+            for (int index = 1; index <= pattern.length(); index++) {
+                y = (y * X_FACTOR) % PRIME;
+            }
+            for (int index = lastSubStrIndex - 1; index >= 0; index--) {
+                hashes[index] = (int) (X_FACTOR * hashes[index + 1] + text.charAt(index) - y * text.charAt(index + pattern.length())) % PRIME;
+            }
+            return hashes;
+        }
+
+        List<Integer> findOccurrencesRabinKarp(String pattern, String text) {
+            List<Integer> occurrences = new ArrayList<>();
+            int patternHash = computeHash(pattern);
+            int[] hashes = preComputeHashes(pattern, text);
+            for (int index = 0; index <= text.length() - pattern.length(); index++) {
+                if (patternHash == hashes[index] && pattern.equals(text.substring(index, index + pattern.length()))) {
+                    occurrences.add(index);
+                }
+            }
+            return occurrences;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        FastScanner in = new FastScanner();
+
+        String pattern = in.next();
+        String text = in.next();
+
+        HashFunction hasher = new HashFunction();
+        List<Integer> occurrences = hasher.findOccurrencesRabinKarp(pattern, text);
+
+        occurrences.forEach(index -> System.out.print(index + " "));
+    }
+
+    static class FastScanner {
+        private BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = null;
+        }
+
+        public String next() throws IOException {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+}
