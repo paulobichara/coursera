cb89eb6ed3acd8e71df6d250a891d084ed1cecd7
Paulo Bichara <contato@paulobichara.org>
2019-04-22 15:32:37 -0300

    First challenge completed

---
+++ src/main/java/BreadthFirstSearch.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BreadthFirstSearch {
+
+    private static class Node {
+        List<Node> neighbours;
+        int value;
+
+        Node(int value) {
+            this.value = value;
+            neighbours = new ArrayList<>();
+        }
+
+        void addNeighbour(Node node) {
+            neighbours.add(node);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof Node)) {
+                return false;
+            }
+            return this.value == ((Node)obj).value;
+        }
+    }
+
+    private static class Graph {
+        private Node[] nodes;
+
+        Graph(int qtyNodes) {
+            nodes = new Node[qtyNodes];
+            for (int index = 0; index < qtyNodes; index++) {
+                nodes[index] = new Node(index);
+            }
+        }
+
+        Node getNode(int index) {
+            if (nodes[index] == null) {
+                nodes[index] = new Node(index);
+            }
+            return nodes[index];
+        }
+
+        int minimumEdges(int from, int to) {
+            int[] distances = new int[nodes.length];
+            Arrays.fill(distances, Integer.MAX_VALUE);
+            distances[from] = 0;
+
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(from);
+            while (!queue.isEmpty()) {
+                Node current = nodes[queue.poll()];
+                for (Node neighbour : current.neighbours) {
+                    if (distances[neighbour.value] == Integer.MAX_VALUE) {
+                        queue.add(neighbour.value);
+                        distances[neighbour.value] = distances[current.value] + 1;
+                    }
+                }
+            }
+            return distances[to] == Integer.MAX_VALUE ? -1 : distances[to];
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Graph graph = new Graph(scanner.nextInt());
+
+        int qtyEdges = scanner.nextInt();
+        for (int i = 0; i < qtyEdges; i++) {
+            int firstIndex = scanner.nextInt() - 1;
+            int secondIndex = scanner.nextInt() - 1;
+            graph.getNode(firstIndex).addNeighbour(graph.getNode(secondIndex));
+            graph.getNode(secondIndex).addNeighbour(graph.getNode(firstIndex));
+        }
+        int fromIndex = scanner.nextInt() - 1;
+        int toIndex = scanner.nextInt() - 1;
+
+        System.out.println(graph.minimumEdges(fromIndex, toIndex));
+    }
+
+}
