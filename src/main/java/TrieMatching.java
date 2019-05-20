fe7790a12adc560cc10577900d05437e0a238289
Paulo Augusto Dacach Bichara <contato@paulobichara.org>
2019-05-20 16:35:09 -0300

    Challenge completed

---
+++ src/main/java/TrieMatching.java
@@ -0,0 +1,68 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class TrieMatching {
+    private static class Node {
+        Map<Character, Node> outgoing;
+
+        Node() {
+            outgoing = new HashMap<>();
+        }
+    }
+
+    private static class Trie {
+
+        Node root;
+
+        Trie(String[] patterns) {
+            root = new Node();
+            for (String pattern : patterns) {
+                Node current = root;
+                for (int index = 0; index < pattern.length(); index++) {
+                    char currentSymbol = pattern.charAt(index);
+                    if (current.outgoing.containsKey(currentSymbol)) {
+                        current = current.outgoing.get(currentSymbol);
+                    } else {
+                        Node node = new Node();
+                        current.outgoing.put(currentSymbol, node);
+                        current = node;
+                    }
+                }
+            }
+        }
+
+        void matches(String text) {
+            for (int index = 0; index < text.length(); index++) {
+                char currentSymbol = text.charAt(index);
+                Node currentNode = root;
+
+                for (int currentIndex = index; currentIndex < text.length() && !currentNode.outgoing.isEmpty()
+                        && currentNode.outgoing.containsKey(currentSymbol); currentIndex++) {
+                    currentNode = currentNode.outgoing.get(currentSymbol);
+                    currentSymbol = currentIndex + 1 < text.length() ? text.charAt(currentIndex + 1) : '\u0000';
+                }
+
+                if (currentNode.outgoing.isEmpty()) {
+                    System.out.print(index + " ");
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            String text = scanner.next();
+
+            int qtyPatterns = scanner.nextInt();
+            String[] patterns = new String[qtyPatterns];
+
+            for (int index = 0; index < qtyPatterns; index++) {
+                patterns[index] = scanner.next();
+            }
+
+            new Trie(patterns).matches(text);
+        }
+    }
+}
